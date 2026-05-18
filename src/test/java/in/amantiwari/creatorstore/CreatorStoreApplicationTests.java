4209aa689180db33e84da073bfb2392393ecc641
AmanTiwari2304 <amant9363@gmail.com>
2026-05-18 14:21:20 +0530

    Basic Setup

---
+++ src/test/java/in/amantiwari/creatorstore/CreatorStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package in.amantiwari.creatorstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
